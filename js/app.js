777779e33502427c9de767fb94c8ad0d095624e7
Dario Castañé <i@dario.im>
2019-03-18 23:49:03 +0100

    Agenda feta i disseny adaptat

--- js/app.js
+++ js/app.js
@@ -1,1 +1,130 @@
 $(document).foundation()
+const geoMarks = {
+    "Lleida": [
+        {
+            "title": "<b>Ateneu La Baula</b><hr /><b>Carrer del Nord, 4, 25007. LLeida</b><br />De dilluns a divendres<br />De 9.00 a 23.00<br />Dissabtes de 12.00 a 23.00<br />Diumenge tancat<br /><a target=_blank href=\"https://maps.google.com/maps/search/Ateneu La Baula/@41.6188116,0.6301967\">Obrir a Google Maps</a>",
+            "position": "41.6188116,0.6301967"
+        }
+    ],
+    "Barcelona": [
+        {
+            "position": "41.6166541,0.6206359",
+            "title": "<b>Epap Bar</b><hr /><b>Carrer Panera, 5, 25002 Lleida</b><br /><a target=_blank href=\"https://maps.google.com/maps/search/@41.6166541,0.6206359\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.4350282,2.1682971",
+            "title": "<b>Casal Tres Voltes Rebel</b><hr /><b>Passeig d'Urrutia, 125. 08042 Barcelona</b><br />De dilluns a divendres de 17.30 a 21.00.<br /><a target=_blank href=\"https://maps.google.com/maps/search/Casal Tres Voltes Rebel/@41.4350282,2.1682971\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.565497,2.0180183",
+            "title": "<b>Botiga de Cultiu L'AglÃ </b><hr /><b>Carrer Salmeron 66, Baixos, 08222 Terrassa</b><br />Matins de 11.00 a 14.00<br />Tardes de 17.00 a 20.30<br />Dilluns matÃ­ tancat.<br /><a target=_blank href=\"https://maps.google.com/maps/search/Botiga de Cultiu L'AglÃ /@41.565497,2.0180183\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.3664528,2.1256128",
+            "title": "<b>Ateneu Popular i Independentista La Iaia</b><hr /><b>Carrer Jacint Verdaguer, 14, 08902. Hospitalet de Llobregat</b><br />De dilluns a dissabte<br />Desde les 18.00 a 21.00<br />Diumenge tancat<br /><a target=_blank href=\"https://maps.google.com/maps/search/Ateneu Popular i Independentista La Iaia/@41.3664528,2.1256128\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.3780543,2.1413874",
+            "title": "<b>Espai Basset</b><hr /><b> Carrer de l'Elisi 20 de Barcelona</b><br />Obert de Dilluns a Dissabte<br />NomÃ©s tardes de 18.00 a 21.00<br /><a target=_blank href=\"https://maps.google.com/maps/search/Espai Basset/@41.378068,2.1413302\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.4501532,2.2452182",
+            "title": "<b>Paradeta CDR</b><hr /><b>PlaÃ§a de la Vila, Badalona</b><br />Dilluns de 19h a 20.30h (paradeta CDR)<br /><a target=_blank href=\"https://maps.google.com/maps/search/Paradeta CDR/@41.4501532,2.2452182\">Obrir a Google Maps</a>"
+        }
+    ],
+    "Girona": [
+        {
+            "position": "41.4472827,2.2423045",
+            "title": "<b>Casal Antoni Sala i Pont</b><hr /><b>C/Colom, 66 - Badalona</b><br />Obert de Dilluns a Divendres<br />Matins de 10.00 a 14.00<br />Tardes de 16.00 a 19.30<br /><a target=_blank href=\"https://maps.google.com/maps/search/Casal Antoni Sala i Pont/@41.4472827,2.2423045\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.984106,2.8226217",
+            "title": "<b>La Pedra</b><hr /><b>Carrer dels Mercaders, 18, 17004 Girona</b><br />19:00 - 00:00 (de dimarts a dissabte)<br /><a target=_blank href=\"https://maps.google.com/maps/search/La Pedra/@41.984106,2.8226217\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.9896027,2.8230739",
+            "title": "<b>El Forn</b><hr /><b>PlaÃ§a de Sant Pere, 5, 17007 Girona</b><br />19:00 - 00:00 (de dimarts a divendres)<br />12:00 - 00:00 (dissabte)<br /><a target=_blank href=\"https://maps.google.com/maps/search/El Forn/@41.9896027,2.8230739\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.9829942,2.8224718",
+            "title": "<b>Llibreria les Voltes</b><hr /><b>PlaÃ§a del Vi, 2, 17004 Girona</b><br />09:00 - 14:00 i 16:00 - 20:00 (de dilluns a divendres)<br />10:00 - 14:00 i 16:00 - 20:00 (dissabte)<br />10:00 - 14:00 (diumenge)<br /><a target=_blank href=\"https://maps.google.com/maps/search/Llibreria les Voltes/@41.9829942,2.8224718\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.9563633,2.6348814",
+            "title": "<b>Bar la repÃºblica</b><hr /><b>PlaÃ§a catalunya, 11, Girona</b><br />AnglÃ¨s<br />Tots els dies de 09:00 a 24:00<br /><a target=_blank href=\"https://maps.google.com/maps/search/Bar la repÃºblica/@41.9563633,2.6348814\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.9558196,2.6353911",
+            "title": "<b>Llibreria Nati</b><hr /><b>IndÃºstria 22, Girona</b><br />AnglÃ¨s<br />Matins de 9.00 a 1.00<br />Tardes de 5.00 a 7.00<br /><a target=_blank href=\"https://maps.google.com/maps/search/Llibreria Nati/@41.9558196,2.6353911\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.8636125,2.6604282",
+            "title": "<b>Estanc Can Net</b><hr /><b>PlaÃ§a Farners. Santa Coloma de Farners</b><br /><a target=_blank href=\"https://maps.google.com/maps/search/Estanc Can Net/@41.8636125,2.6604282\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.7393455,2.884346",
+            "title": "<b>Paradeta al Mercat Setmanal</b><hr /><b>Mercat Setmanal de Tossa de Mar, Girona</b><br />Dijous matÃ­<br /><a target=_blank href=\"https://maps.google.com/maps/search/Paradeta al Mercat Setmanal/@41.7393455,2.884346\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.9595699,3.0363085",
+            "title": "<b>Ca La Pilar Dumingu</b><hr /><b>PlaÃ§a Major, 12, La Bisbal d'EmpordÃ </b><br />Obert de Dimarts a Dissabte<br />Desde les 9.00 fins les 00.00<br /><a target=_blank href=\"https://maps.google.com/maps/search/Ca La Pilar Dumingu/@41.9595699,3.0363085\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "42.2682984,2.9545769",
+            "title": "<b>Bar La Cate</b><hr /><b>Ronda Rector Aroles, 4. Figueres</b><br />https://barlacate.cat<br /><a target=_blank href=\"https://maps.google.com/maps/search/Bar La Cate/@42.2682984,2.9545769\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.579672,2.5509549",
+            "title": "<b>Merceria La Troca</b><hr /><b>Riera Bisbe Pol 116, Arenys de Mar</b><br /><a target=_blank href=\"https://maps.google.com/maps/search/\@41.579672,2.5509549\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.5348421,2.2150054",
+            "title": "<b>Casal Popular El Tabaran</b><hr /><b>Carrer de Berenguer III, 162, Mollet del VallÃ¨s</b><br /><a target=_blank href=\"https://maps.google.com/maps/search/\@41.5350482,2.215424\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "42.011146,2.0266423",
+            "title": "<b>Taverna El Caminet</b><hr /><b>Carrer Bruc, 35, Prats de LluÃ§anÃ¨s</b><br /><a target=_blank href=\"https://maps.google.com/maps/search/\@42.011146,2.0266423\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.230075,0.5486593",
+            "title": "<b>Cerveseria Dresden</b><hr /><b>Av. de Catalunya, 16, Flix</b><br /><a target=_blank href=\"https://maps.google.com/maps/search/\@41.230075,0.5486593\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "40.5986482,0.4428078",
+            "title": "<b>Pub Glops</b><hr /><b>Carrer GuifrÃ©, 26, Ulldecona</b><br /><a target=_blank href=\"https://maps.google.com/maps/search/\@40.5986482,0.4428078\">Obrir a Google Maps</a>"
+        },
+        {
+            "position": "41.5374676,2.1168129",
+            "title": "<b>Centre CÃ­vic Sant Oleguer</b><hr /><b>Carrer de Sol i PadrÃ­s 93, Sabadell</b><br /><a target=_blank href=\"https://maps.google.com/maps/search/\@41.5374676,2.1168129\">Obrir a Google Maps</a>"
+        },
+
+        {
+            "position": "41.4932188,2.1346183",
+            "title": "<b>Local CompromÃ­s per Cerdanyola</b><hr /><b>C/ Pintor Togores 42, Cerdanyola del VallÃ¨s</b><br />Si noo estÃ  obert, es poden deixar a la bÃºstia amb el logo de CxC<br /><a target=_blank href=\"https://maps.google.com/maps/search/@41.4932188,2.1346183\">Obrir a Google Maps</a>"
+        },
+
+        {
+            "position": "41.4915989,2.1379718",
+            "title": "<b>PlaÃ§a Abat Oliva</b><hr /><b>Cerdanyola del VallÃ¨s</b><br />De dilluns a dijous, de 10 a 2 hores i de 16 a 18 hores<br /><a target=_blank href=\"https://maps.google.com/maps/search/@41.4915989,2.1379718\">Obrir a Google Maps</a>"
+        }
+        ,
+
+        {
+            "position": "41.5721014,2.2293384",
+            "title": "<b>Perruqueria RÃ­nxols</b><hr /><b>Av. LluÃ­s Companys, 25, Parets del VallÃ¨s</b><br />De 9:30 a 13 hores, i de 15 a 19 hores<br /><a target=_blank href=\"https://maps.google.com/maps/search/@41.5721014,2.2293384\">Obrir a Google Maps</a>"
+        }
+        ,
+
+        {
+            "position": "41.12106,1.2506506",
+            "title": "<b>Fruites i verdures Ton</b><hr /><b>Carrer de Florenci Vives 6, Tarragona</b><br />De 9:30 a 13 hores, i de 15 a 19 hores<br /><a target=_blank href=\"https://maps.google.com/maps/search/@41.12106,1.2506506\">Obrir a Google Maps</a>"
+        }
+        ,
+
+        {
+            "position": "41.7300081,1.8177942",
+            "title": "<b>Espai CAT</b><hr /><b>Carrer de Lleida 10, Manresa</b><br />De 9 a 19 hores<br /><a target=_blank href=\"https://maps.google.com/maps/search/@41.7300081,1.8177942\">Obrir a Google Maps</a>"
+        }
+    ]
+};
+
