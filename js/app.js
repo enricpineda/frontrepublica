86f42bf53d1793a7a25619eb1864f7c25e0b052f
Enric Pineda <enricpineda@gmail.com>
2019-03-20 15:30:28 +0100

    Nous punts de signatures

--- js/app.js
+++ js/app.js
@@ -145,6 +145,11 @@ const geoMarks = {
       "position": "41.5415582,2.4432095",
       "title": "<b>Casal de la Nova Aliança</b><hr /><b>Carrer Bonaire, 25, 08301 Mataró</b><br />De 17 a 22h<br /><a target=_blank href=\"https://maps.google.com/maps/search/@41.5415582,2.4432095\">Obrir a Google Maps</a>"
     }
+      ,
+      {
+      "position": "41.5052808,2.3436674",
+      "title": "<b>Premià de Dalt</b><hr /><b>Carretera d'enllaç 46, Premià de Dalt</b><br />Dimecres 20, de 18 a 20h<br /><a target=_blank href=\"https://maps.google.com/maps/search/@41.5052808,2.3436674\">Obrir a Google Maps</a>"
+    }
 
   ],
   "Girona": [
